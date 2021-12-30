23d6fef1aab5e4952909f2f057db6b1c0a3fa989
areirei <xiwanwu@hotmail.com>
2021-12-30 11:43:13 +0800

    Fix

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -3,15 +3,14 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-;
+    ->in(__DIR__.DIRECTORY_SEPARATOR.'src')
+    ->in(__DIR__.DIRECTORY_SEPARATOR.'tests')
+    ->append(['.php_cs.dist.php']);
 
 $rules = [
     '@Symfony' => true,
 ];
 $config = new PhpCsFixer\Config();
-$config->setRules($rules)
-    ->setFinder($finder)
-;
 
-return $config;
+return $config->setRules($rules)
+    ->setFinder($finder);
